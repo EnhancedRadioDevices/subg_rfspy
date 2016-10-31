89526b7af50b4ba43047518f87d89b02eac2b1de
nick6x <nick.6.x@gmail.com>
2016-10-31 16:38:49 -0700

    Fix timeout ISR enable/disable placement.

--- spi1_alt2/serial.c
+++ spi1_alt2/serial.c
@@ -109,13 +109,14 @@ void t4_isr(void) __interrupt T4_VECTOR { //
 void rx1_isr(void) __interrupt URX1_VECTOR {
   uint8_t value;
   value = U1DBUF;
-  T4CTL |= 0x0C; // Enable Timer 4 Overflow Interrupt Mask, clear counter
-  T4OVFIF = 0; // Clear Timer 4 Overflow flag
-  T4IF = 0; // Clear Timer 4 Interrupt flag
-  T4CTL |= 0x10; // Start Timer 4
-  T4IE = 1;		// Enable T4IE interrupt
+  T4CTL |= 0x04; // Clear Timer 4 counter
 
   if (spi_mode == SPI_MODE_WAIT && value == 0x99) {
+	T4CTL |= 0x0C; // Enable Timer 4 Overflow Interrupt Mask, clear counter
+    T4OVFIF = 0; // Clear Timer 4 Overflow flag
+    T4IF = 0; // Clear Timer 4 Interrupt flag
+    T4CTL |= 0x10; // Start Timer 4
+    T4IE = 1;		// Enable T4IE interrupt
     if (ready_to_send) {
       slave_send_size = output_size;
       ready_to_send = 0;
@@ -124,7 +125,6 @@ void rx1_isr(void) __interrupt URX1_VECTOR {
     }
     spi_mode = SPI_MODE_SIZE;
     U1DBUF = slave_send_size;
-	T4IE = 0;		// Disable T4IE interrupt
     return;
   }
 
@@ -134,8 +134,8 @@ void rx1_isr(void) __interrupt URX1_VECTOR {
       spi_mode = SPI_MODE_XFER;
     } else {
       spi_mode = SPI_MODE_WAIT;
+	  T4IE = 0;		// Disable T4IE interrupt
     }
-	T4IE = 0;		// Disable T4IE interrupt
     return;
   }
 
@@ -150,14 +150,15 @@ void rx1_isr(void) __interrupt URX1_VECTOR {
       if (input_size == master_send_size) {
         master_send_size = 0;
         serial_data_available = 1;
-		spi_mode = SPI_MODE_WAIT; //
+		spi_mode = SPI_MODE_WAIT;
+		T4IE = 0;
       }
     }
     if (slave_send_size == 0 && master_send_size == 0) {
       spi_mode = SPI_MODE_WAIT;
+	  T4IE = 0;
     }
   }
-  T4IE = 0;		// Disable T4IE interrupt
 }
 
 void tx1_isr(void) __interrupt UTX1_VECTOR {
@@ -167,6 +168,7 @@ void tx1_isr(void) __interrupt UTX1_VECTOR {
       slave_send_size--;
       if (slave_send_size == 0 && master_send_size == 0) {
         spi_mode = SPI_MODE_WAIT;
+		T4IE = 0;
       }
       U1DBUF = spi_output_buf[output_tail_idx];
       output_size--;
